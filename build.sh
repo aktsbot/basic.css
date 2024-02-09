93f020304dfabc0bb7cea21025fe18e5954a9e5d
aktsbot <box.ashishk@gmail.com>
2024-02-09 16:09:06 +0530

    856B :)

--- build.sh
+++ build.sh
@@ -48,6 +48,15 @@ do
   awk "$cssCommentRemover" "$cssFile" | tr -s ' ' >> "$TEMP_FILE"
 done
 
+# make foo {}
+# into foo{}
+sed -i 's/\ {/{/g' "$TEMP_FILE"
+# make foo{ prop into foo{prop
+sed -i 's/{ //g' "$TEMP_FILE"
+# make prop: value into prop:value
+sed -i 's/:\ /:/g' "$TEMP_FILE"
+sed -i 's/;\ /;/g' "$TEMP_FILE"
+
 # make everything in oneline
 cat "$TEMP_FILE" | tr -d '\n' >> "$BUILD_FILE"
 
@@ -56,5 +65,5 @@ rm "$TEMP_FILE"
 
 # finding gz size of the bundle now
 GZ_SIZE=$(gzip -9 -c "$BUILD_FILE" | wc -c | numfmt --to=iec-i --suffix=B)
-echo "Bundle size is: $GZ_SIZE"
+echo "Bundle size after gz is: $GZ_SIZE"
 
